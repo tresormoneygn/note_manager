e75418ea7c592e757dbf4f085cecff1b9432e564
koulibalyamadou10 <koulibalyamadou10@gmail.com>
2025-06-21 10:38:20 +0000

    une version de l'application web

--- src/Controller/EtudiantController.php
+++ src/Controller/EtudiantController.php
@@ -9,6 +9,7 @@ use App\Form\EtudiantType;
 use App\Form\ImportRapportType;
 use App\Form\FiltreEtudiantType;
 use App\Repository\EtudiantRepository;
+use App\Repository\MatiereRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -28,7 +29,8 @@ class EtudiantController extends AbstractController
         Request $request,
         PaginatorInterface $paginator,
         Security $security,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        MatiereRepository $matiereRepository
     ): Response {
         $importForm = $this->createForm(ImportRapportType::class, options: [
             'user' => $this->getUser(),
@@ -36,9 +38,11 @@ class EtudiantController extends AbstractController
         ]);
 
         $user = $security->getUser();
+        $userMatieres = $matiereRepository->findBy(['user' => $user]);
+        
         $filtreForm = $this->createForm(FiltreEtudiantType::class, null, [
             'method' => 'GET',
-            'user_matieres' => $user->getMatieres()
+            'user_matieres' => $userMatieres
         ]);
         $filtreForm->handleRequest($request);
 
@@ -65,12 +69,69 @@ class EtudiantController extends AbstractController
             ]
         );
 
+        // Calculate statistics
+        $stats = $this->calculateStudentStatistics($entityManager);
+
         return $this->render('etudiant/index.html.twig', [
             'etudiants' => $etudiants,
             'active_page' => 'etudiant',
             'form' => $importForm->createView(),
             'filtre_form' => $filtreForm->createView(),
+            'gradeDistribution' => $stats['distribution'],
+            'programAverages' => $stats['averages']
         ]);
+    }
+
+    private function calculateStudentStatistics(EntityManagerInterface $em): array
+    {
+        $distribution = [
+            '0-5' => 0,
+            '5-10' => 0,
+            '10-15' => 0,
+            '15-20' => 0
+        ];
+
+        $programTotals = [];
+        $programCounts = [];
+
+        // Get all notes
+        $notes = $em->createQuery('
+            SELECT n.note_1, n.note_2, n.note_3, m.name as matiere
+            FROM App\Entity\Note n
+            JOIN n.matiere m
+        ')->getResult();
+
+        foreach ($notes as $note) {
+            // Calculate average for this note
+            $average = ($note['note_1'] * 0.3 + $note['note_2'] * 0.3 + $note['note_3'] * 0.4);
+            
+            // Update distribution
+            if ($average < 5) $distribution['0-5']++;
+            elseif ($average < 10) $distribution['5-10']++;
+            elseif ($average < 15) $distribution['10-15']++;
+            else $distribution['15-20']++;
+
+            // Update program averages
+            if (!isset($programTotals[$note['matiere']])) {
+                $programTotals[$note['matiere']] = 0;
+                $programCounts[$note['matiere']] = 0;
+            }
+            $programTotals[$note['matiere']] += $average;
+            $programCounts[$note['matiere']]++;
+        }
+
+        // Calculate program averages
+        $programAverages = [];
+        foreach ($programTotals as $program => $total) {
+            $programAverages[$program] = $programCounts[$program] > 0 
+                ? round($total / $programCounts[$program], 1)
+                : 0;
+        }
+
+        return [
+            'distribution' => $distribution,
+            'averages' => $programAverages
+        ];
     }
 
     #[Route('/new', name: 'app_etudiant_new', methods: ['GET', 'POST'])]
